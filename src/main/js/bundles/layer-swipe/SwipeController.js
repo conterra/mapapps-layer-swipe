0fffd09657935dc3138029c6c162eb02a30810eb
Matthias Stein <m.stein@conterra.de>
2017-02-13 14:10:50 +0100

    Reformat code

--- src/main/js/bundles/layer-swipe/SwipeController.js
+++ src/main/js/bundles/layer-swipe/SwipeController.js
@@ -81,14 +81,14 @@ define([
             var vs = d_window.getBox();
 
             if (opts.left) {
-                if (typeof(opts.left) == "string" && opts.left.indexOf("%")) {
+                if (typeof(opts.left) == "string" && opts.left.indexOf("%") !== -1) {
                     var ratioWidth = parseFloat(opts.left.replace("%", "")) / 100;
                     var width = vs.w;
                     opts.left = width * ratioWidth;
                 }
             }
             if (opts.top) {
-                if (typeof(opts.top) == "string" && opts.top.indexOf("%")) {
+                if (typeof(opts.top) == "string" && opts.left.indexOf("%") !== -1) {
                     var ratioHeight = parseFloat(opts.top.replace("%", "")) / 100;
                     var height = vs.h;
                     opts.top = height * ratioHeight;
